91202634708fa59944d0e618e8b55026fb87dddb
LoknathBehera2112 <31297544+LoknathBehera2112@users.noreply.github.com>
2017-08-31 16:02:35 +0530

    Add files via upload

---
+++ 2 c to f.sh
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ float c; float f;
+ cout<<"\n Please enter the desired temperature in celcius";
+ cin>>c;
+ f=c*9/5+32;
+ cout<<"\n The temperature you entered is "<<f<<" in Fahrenheit";
+	return 0;
+}
---
+++ 3 f to c.sh
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ float c; float f;
+ cout<<"\n Please enter the desired temperature in fahrenheit";
+ cin>>f;
+ c=5*(f-32)/9;
+ cout<<"\n The temperature you entered is "<<c<<" in celcious";
+	return 0;
+}
