cd16a6d0a4e261718e4db4d68321060b85ccd3a6
LoknathBehera2112 <31297544+LoknathBehera2112@users.noreply.github.com>
2017-09-09 12:59:59 +0530

    Add files via upload

---
+++ assignment2question5.cpp
@@ -0,0 +1,22 @@
+#include 
+<iostream>
+#include 
+<cmath>
+using namespace std;
+
+
+int main() {
+ int i; int j;
+
+ cout<<"Enter the base number";
+
+ cin>>i;
+ 
+cout<<"Enter the exponent number";
+
+ cin>>j;
+ 
+cout<<pow(i,j);
+
+	
+return 0;
